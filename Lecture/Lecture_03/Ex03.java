df002b12acd3a09f3808b5a4bb7569afbdf14c5e
Artem <shenlin@yandex.ru>
2023-06-29 15:51:03 +0300

    lecture_03-2

--- Lecture/Lecture_03/Ex03.java
+++ Lecture/Lecture_03/Ex03.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Ex03 {
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        ArrayList<Integer> list2 = new ArrayList<>();
+        // ArrayList<Integer> list1 = new ArrayList<Integer>();
+        // ArrayList<Integer> list2 = new ArrayList<>();
         ArrayList<Integer> list3 = new ArrayList<>(10);
         ArrayList<Integer> list4 = new ArrayList<>(list3);
         System.out.println(list3);
