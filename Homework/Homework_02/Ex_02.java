4cedd3867b75262a20ac3601c9f15195ef686dad
Artem <shenlin@yandex.ru>
2023-06-29 19:01:04 +0300

    homework_02_2

--- Homework/Homework_02/Ex_02.java
+++ Homework/Homework_02/Ex_02.java
@@ -21,9 +21,8 @@ public class Ex_02 {
     }
 
     public static int[] bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
